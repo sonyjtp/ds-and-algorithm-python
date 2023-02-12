25fc26d9936baecd2112ec38edefea2a99f7b143
Sony Jacob Thomas <sonyjtp@gmail.com>
2023-02-12 11:26:48 -0600

    two_sum.py

--- common/two_sum.py
+++ common/two_sum.py
@@ -1,8 +1,3 @@
-# https://leetcode.com/problems/two-sum/
-# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-# You may not use the same element twice.
-# You can return the answer in any order.
-
 import random
 
 
