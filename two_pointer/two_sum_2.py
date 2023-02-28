31990f65de895939d89cdd73109bf00b5e62f896
Sony Jacob Thomas <sonyjtp@gmail.com>
2023-02-27 18:37:54 -0600

    two_sum.py

--- two_pointer/two_sum_2.py
+++ two_pointer/two_sum_2.py
@@ -3,12 +3,17 @@ import random
 
 # https://leetcode.com/problems/two-sum/
 def two_sum():
+    found = []
+    added = []
     for i in range(len(nums)):
-        if target - nums[i] in nums and i != nums.index(target - nums[i]):
-            return [i, nums.index(target - nums[i])]
+        if target - nums[i] in nums and nums.index(target - nums[i]) not in added:
+            found.append((i, nums.index(target - nums[i])))
+            added.append(i)
+    return found
 
 
 arr = [random.randint(1, 20) for x in range(1, 20)]
+arr.sort()
 nums = list(set(arr))
 target = random.randint(1, 20)
 print(f'nums: {nums}\ttarget: {target}\n{two_sum()}')
