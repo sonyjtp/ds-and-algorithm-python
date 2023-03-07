d1a0c1d0d852a596d3de1c69402df625598e5071
Sony Jacob Thomas <sonyjtp@gmail.com>
2023-03-06 23:34:01 -0600

    prefix_sum.py

---
+++ prefix_sum/prefix_sum.py
@@ -0,0 +1,30 @@
+# Given an integer array nums, an array queries where queries[i] = [x, y] and an integer limit,
+# return a boolean array that represents the answer to each query.
+# A query is true if the sum of the subarray from x to y is less than limit, or false otherwise.
+# For example, given nums = [1, 6, 3, 2, 7, 2] and queries = [[0, 3], [2, 5], [2, 4]] and limit = 13,
+# the answer is [true, false, true]. For each query, the subarray sums are [12, 14, 12].
+
+import random
+
+
+def prefix_sum():
+    prefix = [nums[0]]
+    result = []
+    for i in range(1, len(nums)):
+        prefix.append(nums[i] + prefix[i - 1])
+    for query in queries:
+        if query[0] == 0:
+            sum_of_subarray = prefix[query[1]]
+        else:
+            sum_of_subarray = prefix[query[1]] - prefix[query[0] - 1]
+        result.append(sum_of_subarray < limit)
+    return result
+
+
+nums = [random.randint(1, 10) for x in range(15)]
+queries = [[random.randint(0, 5), random.randint(5, 9)] for x in range(6)]
+limit = random.randint(20, 30)
+print(nums)
+print(queries)
+print(limit)
+print(prefix_sum())
